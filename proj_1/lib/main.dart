1949897d9479c273be8fd10e9814d5a88d3415bc
Dominique Pearyer <dpear1050@gmail.com>
2024-03-17 23:56:19 -0400

    add delete flashcard button

--- proj_1/lib/main.dart
+++ proj_1/lib/main.dart
@@ -165,6 +165,12 @@ class _FlashCardAppState extends State<MyCard> {
                       onPressed: showNextCard,
                       icon: Icon(Icons.chevron_right),
                       label: Text('')),
+                      IconButton(
+                    onPressed: () {
+                      _deleteFlashcard(_currentIndex);
+                    },
+                    icon: Icon(Icons.delete),
+                  ),
                 ],
               ),
               ElevatedButton(
@@ -254,6 +260,15 @@ floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
     setState(() {
       _currentIndex =
           (_currentIndex - 1 >= 0) ? _currentIndex - 1 : _flashcards.length - 1;
+    });
+  }
+
+  void _deleteFlashcard(int index) {
+    setState(() {
+      _flashcards.removeAt(index);
+      if (_currentIndex >= _flashcards.length) {
+        _currentIndex = _flashcards.length - 1;
+      }
     });
   }
 
